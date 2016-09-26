1f62bd13ac618f9e0a862556a6cfc7612e1e20e9
Sigit Prayoga <sigit.prayoga@obortech.com>
2016-09-26 12:49:14 +0700

    change to staging

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 //define some global variables
 //TODO remove this to config file
-var local = true;
+var local = false;
 var baseUrl = local ? 'http://localhost:1818' : 'http://161.202.180.166:1818';
 (function() {
 	//create a main module for the app with no dependencies yet
