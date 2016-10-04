fe81d52e4e37a66f39f7e4527693536cf983dc17
Sigit Prayoga <sigit.prayoga@obortech.com>
2016-10-04 11:35:40 +0700

    Implement searching swit, and adding moment to show the posting time

--- js/eachSwitDirective/eachSwit.directive.js
+++ js/eachSwitDirective/eachSwit.directive.js
@@ -1,11 +1,24 @@
-(function(){
-	'use strict';
-	angular.module('switrApp').directive('eachSwit', eachSwit);
+(function() {
+    'use strict';
+    angular.module('switrApp').directive('eachSwit', eachSwit);
 
-	function eachSwit(){
-		return {
-			templateUrl: 'js/eachSwitDirective/eachSwit.view.html',
-			restrict: 'E'
-		};
-	}
+    function eachSwit() {
+        return {
+            templateUrl: 'js/eachSwitDirective/eachSwit.view.html',
+            scope: {
+                switObj: '=swit' // '=' 2 way binding, '@' one way binding, '&' for binding a function
+            },
+            controller: function($scope) {
+            	//convert to be like '3 minutes ago'
+                $scope.getMoment = function(date) {
+                	date = new Date(date);
+                	//get the hours from current swit
+                	var hour = date.getHours();
+                	//convert and return
+                    return moment(date).fromNow();
+                };
+            },
+            restrict: 'E' //'E' for element, 'A' for attribute, 'C' for class
+        };
+    }
 })();
